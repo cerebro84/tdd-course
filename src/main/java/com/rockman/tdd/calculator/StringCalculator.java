644b2af012b0a3e86ec3b17c0aa1f7f2854d8905
Angelo Giuseppe De Michele <Angelo.DeMichele@ext.contactlab.com>
2017-03-16 10:45:01 +0100

    Added sum for 2 numbers

--- src/main/java/com/rockman/tdd/calculator/StringCalculator.java
+++ src/main/java/com/rockman/tdd/calculator/StringCalculator.java
@@ -1,7 +1,16 @@
 package com.rockman.tdd.calculator;
 
+import org.springframework.util.StringUtils;
+
 public class StringCalculator {
-    public static int add(String s) {
-        return 0;
+    public static int add(String numbers) {
+        if (StringUtils.isEmpty(numbers)) {
+            return 0;
+        }
+        final String[] numbersArrays = numbers.split(",");
+        if (numbersArrays.length == 1) {
+            return 0;
+        }
+        return Integer.valueOf(numbersArrays[0]) + Integer.valueOf(numbersArrays[1]);
     }
 }
--- src/test/java/com/rockman/tdd/calculator/StringCalculatorTest.java
+++ src/test/java/com/rockman/tdd/calculator/StringCalculatorTest.java
@@ -1,11 +1,13 @@
 package com.rockman.tdd.calculator;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class StringCalculatorTest {
@@ -18,6 +20,32 @@ public class StringCalculatorTest {
     @Test
     public void testReturnZeroIfPassedEmptyString() {
         assertThat("Sum returns 0 for empty string", StringCalculator.add(""), is(0));
+    }
+
+    @Test
+    public void testAddSingleNumber3ShouldReturnTheNumberItself() {
+        assertEquals(0, StringCalculator.add("3"));
+    }
+
+    @Test
+    public void testAddSingleNumber42ShouldReturnTheNumberItself() {
+        assertEquals(0, StringCalculator.add("42"));
+    }
+
+    @Test
+    public void testAddTwoNumbersShouldReturnTheirSum() {
+        assertEquals(42, StringCalculator.add("31,11"));
+    }
+
+    @Test
+    public void testAddTwoNumbers1And2ShouldReturnTheirSum() {
+        assertEquals(3, StringCalculator.add("1,2"));
+    }
+
+    @Test
+    @Ignore
+    public void testAddTwoNumbers1And2ShouldReturnTheirSumVeryBig() {
+        assertEquals(3, StringCalculator.add("19999999999999999999,999999999999999999999999999999992"));
     }
 
 }
