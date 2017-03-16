f81e5a4a4c6f6df785d91abc0e3c99278d0f4d73
Angelo Giuseppe De Michele <Angelo.DeMichele@ext.contactlab.com>
2017-03-16 10:26:40 +0100

    Starting StringCalculator

---
+++ src/main/java/com/rockman/tdd/calculator/StringCalculator.java
@@ -0,0 +1,7 @@
+package com.rockman.tdd.calculator;
+
+public class StringCalculator {
+    public static int add(String s) {
+        return 0;
+    }
+}
--- src/test/java/com/rockman/tdd/calculator/StringCalculatorTest.java
+++ src/test/java/com/rockman/tdd/calculator/StringCalculatorTest.java
@@ -1,4 +1,4 @@
-package com.rockman.tdd;
+package com.rockman.tdd.calculator;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,11 +8,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 @RunWith(MockitoJUnitRunner.class)
-public class Test1 {
+public class StringCalculatorTest {
 
     @Test
     public void testsWork() {
         assertThat("Tests work", true, is(true));
+    }
+
+    @Test
+    public void testReturnZeroIfPassedEmptyString() {
+        assertThat("Sum returns 0 for empty string", StringCalculator.add(""), is(0));
     }
 
 }
